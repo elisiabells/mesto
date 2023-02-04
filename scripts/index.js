b8c49646f6bbae831d114b9d4a988f853516b5fa
Elizaveta Muratova <elisiabells@gmail.com>
2023-02-04 16:47:01 +0400

    .

--- scripts/index.js
+++ scripts/index.js
@@ -3,39 +3,47 @@ const popupContainerEditProfile = document.querySelector('.popup_edit-profile');
 const popupFormEditProfile = document.querySelector('.popup__form-edit');
 
 //      Кнопки 
-const openPopupButton = document.querySelector('.button-open');
+const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__button-close');
 const savePopupButton = document.querySelector('.popup__button-save');
 
 //      Данные в профиле
 const profileInfo = document.querySelector('.profile__info');
-const profileTitle = profileInfo.querySelector('.profile__title-text');
-const profileSubtitle = profileInfo.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title-text');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 //      Popup профиля
 const profileEdit = document.querySelector('.edit-profile');
 const popupInputTypeName = document.querySelector('.popup__input_type_name');
 const popupInputTypeInfo = document.querySelector('.popup__input_type_info');
 
+
+
 //      Открытие формы 
 openPopupButton.addEventListener('click', function () {
-   popupContainerEditProfile.classList.add("popup_opened");
+  popupContainerEditProfile.classList.add("popup_opened");
 });
 
-//      Закрытие формы
+//      Закрытие формы на Х
 closePopupButton.addEventListener('click', function () {
   popupContainerEditProfile.classList.remove("popup_opened");
 });
 
 
-/*   проверка на валидность
-function openProfilePopup() {
-   popupInputTypeName.value = profileTitle.textContent;
-   popupInputTypeInfo.value = profileSubtitle.textContent;
+// Inputы popup профиля
 
-   toggleButtonState(inputList, savePopupButton);
-   inputList.forEach((inputElement) => {
-     checkInputValidity(popupContainerEditProfile, inputElement);
-   });
-   openPopup(popupContainerEditProfile);
- }*/
+function openProfilePopup() {
+  popupInputTypeName.value = profileTitle.textContent;
+  popupInputTypeInfo.value = profileSubtitle.textContent;
+  openPopup(popupContainerEditProfile);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupInputTypeName.value;
+  profileSubtitle.textContent = popupInputTypeInfo.value;
+  closePopup(popupContainerEditProfile);
+}
+
+popupButtonOpen.addEventListener("click", openProfilePopup); //  Открытие попапа изменения профиля
+profileEdit.addEventListener("submit", handleProfileFormSubmit); //   Сохранение данных в профиль
