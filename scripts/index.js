223d54abe667fa371deaab16f92f5d542b66730f
Elizaveta Muratova <elisiabells@gmail.com>
2023-02-07 02:59:16 +0400

    fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,35 @@
+//      Данные из профиля
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
 //      Контейнер popup
 const popupContainerEditProfile = document.querySelector('.popup');
 const popupFormEditProfile = document.querySelector('.popup__form');
 
-//      Кнопки 
+//      кнопки 
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__button-close');
-const savePopupButton = document.querySelector('.popup__button-save');
 
-//      Данные в профиле
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
+//      данные из popup
+const popupInputTypeName = popupFormEditProfile.querySelector('.popup__input_name');
+const popupInputTypeInfo = popupFormEditProfile.querySelector('.popup__input_about');
 
-//      Popup профиля
-const popupInputTypeName = popupFormEditProfile.querySelector('.popup__input-name');
-const popupInputTypeInfo = popupFormEditProfile.querySelector('.popup__input-about');
 
-//    открытие
-openPopupButton.addEventListener('click', function() {
+
+//      функция открытие
+function openPopup() {
   popupContainerEditProfile.classList.add('popup_opened');
-
+  
   popupInputTypeName.value = profileTitle.textContent;
   popupInputTypeInfo.value = profileSubtitle.textContent;
-});
+}
 
-//  закрытие
+//      функция закрытие
 function closePopup() {
   popupContainerEditProfile.classList.remove('popup_opened');
 }
 
-closePopupButton.addEventListener('click', function() {
-  closePopup();
-});
-
+//      функция сохранение
 function saveFormProfileInfo (evt) {
   evt.preventDefault ();
   profileTitle.textContent = popupInputTypeName.value;
@@ -39,4 +37,7 @@ function saveFormProfileInfo (evt) {
   closePopup();
 };
 
+//       слушатели popup
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 popupFormEditProfile.addEventListener ('submit', saveFormProfileInfo);
